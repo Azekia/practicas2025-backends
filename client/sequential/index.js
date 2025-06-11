dd94a0115b43d863d2299eeb88614d6e34772719
Antonio Begines <antonio@begines.net>
2025-06-11 21:11:34 +0200

    estadística de statuses

--- client/sequential/index.js
+++ client/sequential/index.js
@@ -13,6 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
         let minFetchMS = Number.POSITIVE_INFINITY;
         let maxFetchMS = Number.NEGATIVE_INFINITY;
         let histograma = {};
+        let statuses = {};
         const inicio = new Date();
         document.getElementById("resultado").innerText="";
         const intervalID  = setInterval(() => {
@@ -28,23 +29,27 @@ document.addEventListener("DOMContentLoaded", function () {
             + (tiempoTotalFetchMS/numLlamadas).toString() + "ms por fetchs<br>"
             + (minFetchMS).toString() + "ms fetch mas corto<br>"
             + (maxFetchMS).toString() + "ms fetch mas largo<br>"
-            + JSON.stringify(histograma);
+            + '<br>tiempos: ' + JSON.stringify(histograma) 
+            + '<br>statuses: ' + JSON.stringify(statuses);
 
             clearInterval(intervalID );
             return;
           }
           const inicioFetch = new Date();
+          if (interval>=50) console.log("Llamada #" + i);
           fetch(urlAPI, {
             method: "post",
             headers: { 'Content-Type': 'application/json' },
             body:bodyJSON 
           })
           .then((data,err) => {
+            if (interval>=50) console.log("Respuesta #" + i, data.status);
             const finalFetch = new Date();
             const tiempoEsteFetch = finalFetch-inicioFetch;
             if (tiempoEsteFetch<minFetchMS) minFetchMS=tiempoEsteFetch;
             if (tiempoEsteFetch>maxFetchMS) maxFetchMS=tiempoEsteFetch;
             histograma[tiempoEsteFetch] = (histograma[tiempoEsteFetch]||0 ) +1;
+            statuses[data.status] = (statuses[data.status]||0) + 1;
             tiempoTotalFetchMS += tiempoEsteFetch;
           })
           .catch((error) => console.error("Error : ", error));
